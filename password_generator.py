365040af904845a6986c2fb1ee15bf256bf9c3b7
zrk.mrc() <61658876+ziarkomarcin@users.noreply.github.com>
2025-03-25 19:43:45 +0100

    first version of password generator

---
+++ password_generator.py
@@ -0,0 +1,41 @@
+import random
+import string
+
+def generate_password(min_lenght, numbers=True, special_characters=True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+
+    characters = letters
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters += special
+
+    password = ""
+    meets_criteria = False
+    has_number = False
+    has_special = False
+
+    while not meets_criteria or len(password) < min_lenght:
+        new_char = random.choice(characters)
+        password += new_char
+
+        if new_char in digits:
+            has_number = True
+        elif new_char in special:
+            has_special = True
+
+        meets_criteria = True
+        if numbers:
+            meets_criteria = has_number
+        if special_characters:
+            meets_criteria = meets_criteria and has_special
+
+    return password
+
+min_lenght = int(input("Enter the minimum lenght: "))
+has_number = input("Do You want to have numbers? (y/n) ").lower() == "y"
+has_special = input("Do You want to have special characters? (y/n) ").lower() == "y"
+password = generate_password(min_lenght, has_number, has_special)
+print("The generated password is: ", password)
